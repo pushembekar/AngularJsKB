35b91d9dd5cdd2280cc792c58ef9acc412f929a5
Pushkar Shembekar <pshembekar@ostglobal.com>
2017-11-13 11:27:56 -0500

    Starting pagination: Nothing to test

--- Example6/controllers/productListControllers.js
+++ Example6/controllers/productListControllers.js
@@ -1,11 +1,20 @@
 // JavaScript source code
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
-    .controller("productListCtrl", function ($scope, $filter, productListActiveClass) {
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
         var selectedCategory = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
 
         $scope.selectCategory = function (newCategory) {
             selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
         }
 
         $scope.categoryFilterFn = function (product) {
@@ -14,5 +23,9 @@ angular.module("sportsStore")
 
         $scope.getCategoryClass = function (category) {
             return selectedCategory == category ? productListActiveClass : "";
+        }
+
+        $scope.getPageClass = function (page) {
+            return selectedPage == page ? productListActiveClass : "";
         }
     });
