3e94505e2e6ebe4a76087497384b988d0c9379f4
Pushkar Shembekar <pushembekar@gmail.com>
2017-11-12 22:26:13 -0500

    Added category filter

---
+++ Example6/controllers/productListControllers.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+angular.module("sportsStore")
+    .controller("productListCtrl", function ($scope, $filter) {
+        var selectedCategory = null;
+
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+        }
+
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null || product.category == selectedCategory;
+        }
+    });
--- Example6/controllers/sportsStore.js
+++ Example6/controllers/sportsStore.js
@@ -1,12 +1,12 @@
 //add the module
-angular.module("sportStore")
+angular.module("sportsStore")
     .controller("sportStoreCtrl", function ($scope) {
         $scope.data = {
             products: [
                 { name: "Product #1", description: "A product", category: "Category #1", price: 100 }, 
-                { name: "Product #2", description: "B product", category: "Category #2", price: 110 },
-                { name: "Product #3", description: "C product", category: "Category #3", price: 90 },
-                { name: "Product #4", description: "D product", category: "Category #4", price: 140 }
+                { name: "Product #2", description: "B product", category: "Category #1", price: 110 },
+                { name: "Product #3", description: "C product", category: "Category #2", price: 90 },
+                { name: "Product #4", description: "D product", category: "Category #3", price: 140 }
             ]
         };
     });
