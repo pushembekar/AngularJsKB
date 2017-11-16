da130b5b5525c7cb206898e3be657066ed20b65d
Pushkar Shembekar <pushembekar@gmail.com>
2017-11-16 00:21:40 -0500

    Added directive... for some reason 'return' keyword does not like the <Enter> key

--- Example6/components/cart/cart.js
+++ Example6/components/cart/cart.js
@@ -42,35 +42,12 @@ angular.module("cart", [])
             }
         }
     })
-    .directive("cartSummary", function (cart)
-    {
-        return
-        {
-            restrict = "E",
-            templateUrl: "components/cart/cartSummary.html",
-            controller: function($scope)
-            {
+    .directive("cartSummary", function (cart) {
+        return {
+            restrict: "E", templateUrl: "components/cart/cartSummary.html", controller: function ($scope) {
                 var cartData = cart.getProducts();
-
-                $scope.total = function ()
-                {
-                    var total = 0;
-                    for (var i = 0; i < cartData.length; i++)
-                    {
-                        total += (cartData[i].price * cartData[i].count);
-                    }
-                    return total;
-                }
-
-                $scope.itemCount = function ()
-                {
-                    var total = 0;
-                    for (var i = 0; i < cartData.length; i++)
-                    {
-                        total += cartData[i].count;
-                    }
-                    return total;
-                }
+                $scope.total = function () { var total = 0; for (var i = 0; i < cartData.length; i++) { total += (cartData[i].price * cartData[i].count); } return total; }
+                $scope.itemCount = function () { var total = 0; for (var i = 0; i < cartData.length; i++) { total += cartData[i].count; } return total; }
             }
         };
     })
