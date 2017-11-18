b45572854f8349d6827072c5f592099c30ca6a09
Pushkar Shembekar <pushembekar@gmail.com>
2017-11-18 01:49:50 -0500

    Code for copying data to database

--- Example6/controllers/sportsStore.js
+++ Example6/controllers/sportsStore.js
@@ -1,7 +1,9 @@
 //add the module
 angular.module("sportsStore")
     .constant("dataUrl", "http://localhost:2403/products/")
-    .controller("sportStoreCtrl", function ($scope, $http, dataUrl) {
+    .constant("orderUrl", "http://localhost:2403/orders/")
+    .controller("sportStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart)
+    {
         $scope.data = {};
         // the success function
         function successHandler(response) {
@@ -14,4 +16,21 @@ angular.module("sportsStore")
 
         $http.get(dataUrl)
             .then(successHandler, errorHandler);
+
+        $scope.sendOrder = function (shippingDetails)
+        {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .then(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .catch(function (error) {
+                    $scope.data.orderError = error;
+                })
+                .finally(function () {
+                    $location.path("/complete");
+                });
+        }
     });
