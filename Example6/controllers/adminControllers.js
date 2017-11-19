0743f09a1424911c2faf3b16a170fedd605f7925
Pushkar Shembekar <pushembekar@gmail.com>
2017-11-19 01:44:30 -0500

    Finished orders admin page

--- Example6/controllers/adminControllers.js
+++ Example6/controllers/adminControllers.js
@@ -1,6 +1,7 @@
 // JavaScript source code
 angular.module("sportsStoreAdmin")
     .constant("authUrl", "http://localhost:2403/users/login")
+    .constant("ordersUrl", "http://localhost:2403/orders")
     .controller("authCtrl", function ($scope, $http, $location, authUrl)
     {
         $scope.authenticate = function (user, pass)
@@ -24,6 +25,29 @@ angular.module("sportsStoreAdmin")
 
         $scope.getScreen = function () {
             return $scope.current == "Products" ? "views/adminProducts.html" : "views/adminOrders.html";
+        }
+    })
+    .controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+        $http.get(ordersUrl, { withCredentials: true })
+            .then(function (response) {
+                $scope.orders = response.data;
+            })
+            .catch(function (response) {
+                $scope.error = response.error;
+            });
+        $scope.selectedOrder;
+
+        $scope.selectOrder = function (order) {
+            $scope.selectedOrder = order;
+        };
+
+        $scope.calcTotal = function (order) {
+            var total = 0;
+            for (var i = 0; i < order.products.length; i++)
+            {
+                total += order.products[i].price * order.products[i].count;
+            }
+            return total;
         }
     })
     ;
