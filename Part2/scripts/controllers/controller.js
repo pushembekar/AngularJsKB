8e072e12722367fbfd65bc8a290c56d237f5b78a
Pushkar Shembekar <pshembekar@ostglobal.com>
2017-11-21 08:34:28 -0500

    service and module life cycles

---
+++ Part2/scripts/controllers/controller.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+var controllersModule = angular.module("exampleApp.Controllers", []);
+    controllersModule.controller("dayCtrl", function ($scope, days) {
+        $scope.day = days.today;
+    });
+
+    controllersModule.controller("tomorrowCtrl", function ($scope, days) {
+        $scope.day = days.tomorrow;
+    });
---
+++ Part2/scripts/filters/filter.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+angular.module("exampleApp.Filters", [])
+    .filter("dayName", function () {
+        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        return function (input) {
+            return angular.isNumber(input) ? dayNames[input] : input;
+        };
+    });
