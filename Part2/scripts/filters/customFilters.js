a379b2807664a68d77b4e16e78cf28dc0bbc90df
Pushkar Shembekar <pshembekar@ostglobal.com>
2017-12-04 14:43:28 -0500

    custom filter

---
+++ Part2/scripts/filters/customFilters.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+angular.module("exampleApp")
+    .filter("labelCase", function () {
+        return function (value, reverse) {
+            if (angular.isString(value)) {
+                var intermediate = reverse ? value.toUpperCase() : value.toLowerCase();
+                return (reverse ? intermediate[0].toLowerCase() : intermediate[0].toUpperCase()) + intermediate.substr(1);
+            }
+            else {
+                return value;
+            }
+        }
+    });
