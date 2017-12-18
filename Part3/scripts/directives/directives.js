3649890f7f474c44267dbbc032affe6855b95395
Pushkar Shembekar <pshembekar@ostglobal.com>
2017-12-18 12:58:03 -0500

    Moving directive to separate file

---
+++ Part3/scripts/directives/directives.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+angular.module("exampleApp")
+    .directive("triButton", function () {
+        return {
+            scope: { counter: "=counter" },
+            link: function (scope, element, attrs) {
+                element.on("click", function (event) {
+                    console.log("Button click: " + event.target.innerText);
+                    scope.$apply(function () {
+                        scope.counter++;
+                    });
+                });
+            }
+        }
+    });
